18bc7932d9ffb61a07f46f5192e9d2072b557d7d
Kalil Peixoto <kalilmvp@gmail.com>
2020-07-18 21:18:00 +0100

    - Commit of the messenger clone

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FormControl, IconButton, Input } from "@material-ui/core";
+import { Send } from "@material-ui/icons";
+import firebase from "firebase";
+import React, { FormEvent, useCallback, useEffect, useState } from "react";
+import FlipMove from "react-flip-move";
+import "./App.css";
+import Message from "./components/Message";
+import db from "./utils/firebase";
 
-function App() {
+interface MessageData {
+  text: string;
+  user: string;
+}
+
+interface MessageListData {
+  id: string;
+  data: MessageData;
+}
+
+const App: React.FC = () => {
+  const [inputMessage, setInputMessage] = useState("");
+  const [userName, setUserName] = useState<string>("");
+  const [messages, setMessages] = useState<MessageListData[]>([]);
+
+  useEffect(() => {
+    db.collection("messages")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setMessages(
+          snapshot.docs.map((doc) => {
+            return {
+              id: doc.id,
+              data: doc.data(),
+            } as MessageListData;
+          })
+        );
+      });
+  }, []);
+
+  useEffect(() => {
+    const userName = prompt("Enter your username: ");
+    if (userName) {
+      setUserName(userName);
+    }
+  }, []);
+
+  const handleSendMessage = useCallback(
+    (e: FormEvent) => {
+      e.preventDefault();
+
+      db.collection("messages").add({
+        text: inputMessage,
+        user: userName,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      });
+
+      setInputMessage("");
+    },
+    [inputMessage, userName]
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src="https://facebookbrand.com/wp-content/uploads/2018/09/Header-e1538151782912.png?w=100&h=100" />
+      <h1>Heeey Messenger Clone 🚀!</h1>
+      <h2>Username: {userName}</h2>
+
+      <form className="app__form">
+        <FormControl className="app__formControl">
+          <Input
+            className="app__input"
+            type="text"
+            placeholder="Enter a message..."
+            value={inputMessage}
+            onChange={(e) => setInputMessage(e.target.value)}
+          />
+
+          <IconButton
+            type="submit"
+            className="app__iconButton"
+            disabled={!inputMessage}
+            color="primary"
+            onClick={handleSendMessage}
+          >
+            <Send />
+          </IconButton>
+        </FormControl>
+      </form>
+
+      <FlipMove>
+        {messages.map((message, index) => (
+          <Message
+            key={message.id}
+            message={message.data}
+            sender={{ name: userName }}
+          />
+        ))}
+      </FlipMove>
     </div>
   );
-}
+};
 
 export default App;
